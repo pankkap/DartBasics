3f3d02cc044055f827ceb70748788fea586e326c
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-06-04 18:46:42 +0530

    added a note for var VS dynamic
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- 2-variablesDemo.dart
+++ 2-variablesDemo.dart
@@ -1,9 +1,30 @@
-// How to Declare Variables in Dart Language
+/*
+-----var vs dynamic-----
+
+If a variable is declared as a dynamic, its type can change over time.
+
+dynamic a = 'abc'; //initially it's a string
+a = 123; //then we assign an int value to it
+a = true; //and then a bool
+If you declare variable as a var, once assigned type can not change.
+
+var b = 'cde'; //b is a string, and its type can not change
+b = 123; //this will not compile - (can not assign an int to the string variable)
+BUT, if you state a var without initializing, it becomes a dynamic:
+
+var a; //this is actually a dynamic type
+a = 2; //assigned an int to it
+a = 'hello!'; //assigned a string to it
+print(a); //prints out 'hello'
+ */
+
 // Single-Line Comment
+
 /* -------
 Multiline Comment
 ----------
- */
+*/
+
 // ctrl + /  (Short-cut for making comment and uncomment)
 
 void main()
