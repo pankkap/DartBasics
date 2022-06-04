ff36434bb66438de2ce429e1f5468a573db6bc25
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-06-04 18:46:52 +0530

    changed var declaration to dynamic declaration to avoid double to int casting problem removed dead code
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- 7-Operatoes.dart
+++ 7-Operatoes.dart
@@ -7,40 +7,40 @@ void main() {
   3. Relational Operators
   4. Logical Operators
   5. Conditional Operators
-  6. Type Test Operator
-  
+  6. Type Test Operator 
   */
-  var v1 = 10;
-  var v2 = 20;
+
+  // dynamic v1 = 10;
+  // dynamic v2 = 20;
 
   // Arithmatic Operator
   // Binary Operator
-  // print("v1+v2 = ${v1 + v2}");   // Addition
-  // print("v1-v2 = ${v1 - v2}");   // Subtraction
-  // print("v1*v2 = ${v1 * v2}");   // Multiplication
-  // print("v1/v2 = ${v1 / v2}");   // Division
-  // print("v1%v2 = ${v1 % v2}");   // Modulas
-  // print("v1~/v2 = ${v1 ~/ v2}");   // Integer Division
+  // print("v1+v2 = ${v1 + v2}"); // Addition
+  // print("v1-v2 = ${v1 - v2}"); // Subtraction
+  // print("v1*v2 = ${v1 * v2}"); // Multiplication
+  // print("v1/v2 = ${v1 / v2}"); // Division
+  // print("v1%v2 = ${v1 % v2}"); // Modulas
+  // print("v1~/v2 = ${v1 ~/ v2}"); // Integer Division
 
   // Unary Operators
-  // print(v1++); //PostFix Operator 30-->31--->32
-  // print(++v1); //PreFix Operator 32
+  // print(v1++); // PostFix Operator 30-->31--->32
+  // print(++v1); // PreFix Operator 32
   // print(v1--); // Post Decrement 32--->31 --->30
   // print(--v1); // Pre Decrement 30
-  // print(-(-v1));  // Unary (-)Minus Operator   30
+  // print(-(-v1)); // Unary (-) Minus Operator   30
 
-// Assignment Operator
+  // Assignment Operator
   // v1 += 10; // v1 = v1+10
   // v1 -= 10;
   // v1 *= 10;
-  // // v1 /= 10; This will not work  double cannot be assigned to int
+  // v1 /= 10;
   // v1 %= 10;
   // v1 ~/= 10;
   // print(v1);
 
   // Relational Operator
   // var res = v1 < v2;
-  // print("result is: "+ res.toString());
+  // print("result is: " + res.toString());
   // res = v1 > v2;
   // print("result is: $res");
   // res = v1 >= v2;
@@ -51,19 +51,18 @@ void main() {
   // print("result is: $res");
 
   // Logical Operators
-  bool x = true;
-  bool y = false;
-  // var z = (v1 < v2) && x && (v1 > v2); //false
-  // var z1 = (v1 < v2) || x || (v1 > v2); //true
-  // var z2 = !(x || y);
+  // var x = true;
+  // var y = false;
+  // var z = (v1 < v2) && x && (v1 > v2); // false
+  // var z1 = (v1 < v2) || x; // true
+  // var z2 = !x;
   // print(z2);
 
-  int num = 10;
-  String name = "Pankaj";
-  bool isAdmin = true;
+  var num = 10;
+  var name = "Pankaj";
+  var isAdmin = true;
   var abc = 20;
-  print(num is! int);  // false
+  print(num is! int); // false
   print(name is! int); // true
-  print(abc is int);  // true 
-
+  print(abc is int); // true
 }
