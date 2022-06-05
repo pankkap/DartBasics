cb969f11a4578231ec4b7361819d184f27fe2b3b
harsh-sharma-z <harsh.ramakant.sharma@gmail.com>
2022-06-05 12:09:50 +0530

    Updated Code

--- 4-ListDataType.dart
+++ 4-ListDataType.dart
@@ -1,6 +1,6 @@
 // ListDataType
-void main(){
-  List myList = [1,2,3,4,5,"pankaj", true];
+void main() {
+  List myList = [1, 2, 3, 4, 5, "Harsh", true];
   print(myList);
   print(myList[2]);
   print(myList.length);
@@ -8,13 +8,12 @@ void main(){
   print(myList);
   myList.insert(0, 10);
   print(myList);
-  myList.addAll([55,88]);
+  myList.addAll([55, 88]);
   print(myList);
-  myList.insertAll(1, [11,22]);
+  myList.insertAll(1, [11, 22]);
   print(myList);
   myList.remove(55);
   print(myList);
-
 
   myList.forEach((i) {
     print(i);
