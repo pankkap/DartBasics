ef4a7e54e5daaf958e55945881c44392ac01763a
pankkap <pankkap@gmail.com>
2022-06-09 15:14:39 +0530

    Day-8 Code

---
+++ Lesson-3/OOPS/7-Inheritance.dart
@@ -0,0 +1,24 @@
+// Inheritance: Its one of Principle of OOP concept
+
+class Data {
+  String name = "Manish";
+  int age = 25;
+  void details() {
+    print("Name : ${name}");
+    print("Age : ${age}");
+  }
+}
+
+// Inheritance
+class Student extends Data {
+  int mob = 354943534;
+  void showMobile() {
+    print("Mobile No: ${mob}");
+  }
+}
+
+void main() {
+  Student s1 = new Student();
+  s1.details();
+  s1.showMobile();
+}
